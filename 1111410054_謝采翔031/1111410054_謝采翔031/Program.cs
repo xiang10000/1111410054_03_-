c373fc295b10fd5b5c1981eeef73c27306503db0
xiang10000 <155428858+xiang10000@users.noreply.github.com>
2024-04-13 21:39:38 +0800

    Add files via upload

---
+++ 1111410054_謝采翔031/1111410054_謝采翔031/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _1111410054_謝采翔031
+{
+    internal class Program
+    {       
+        static  IList<int> FindWords(string[] words, char x)
+        {
+             var list = new List<int>();
+             for (var i = 0; i < words.Length; i++)
+             {
+                if (!string.IsNullOrEmpty(words[i]) && words[i].Contains(x)) //檢查字串是否有效以及有沒有包含字元x
+                    list.Add(i);
+             }
+             return list ;
+        }        
+        static void Main(string[] args)
+
+        {
+            string[] words = new string[50];
+            for(int i = 0; i < words.Length; i++)    // 讓使用者輸入字串時，知道輸入到第幾個
+            {
+                Console.WriteLine($"請輸入第 {i + 1} 個字串：");
+                words[i] = Console.ReadLine().ToLower();              
+                if (string.IsNullOrEmpty(words[i])) // 當輸入為空值時，跳出迴圈
+                {
+                    break;
+                }
+            }            
+
+            Console.WriteLine("請輸入一個字元: ");
+            char x = Convert.ToChar(Console.ReadLine().ToLower()[0]);   
+            int[] indices = FindWords(words,x).ToArray();
+            Console.WriteLine("輸入字元出現位置： ");            
+            Console.Write("[");
+            foreach (int idx in indices)
+            {
+                Console.Write($"{idx}");
+                if (idx < indices.Length - 1) //為了像題目給的輸出一樣才寫的，用","區隔索引
+                {
+                    Console.Write(",");
+                }
+            }
+            Console.WriteLine("]"); 
+            Console.Read();
+        }   
+    }
+}
---
+++ 1111410054_謝采翔032/1111410054_謝采翔032/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1111410054_謝采翔032
+{
+    internal class Program
+    {
+        static string IntToRoman(int number) //依照題目所給的羅馬數字對應數字來轉換
+        {
+            if (number >= 1000) return "M" + IntToRoman(number - 1000);
+            if (number >= 900) return "CM" + IntToRoman(number - 900);
+            if (number >= 500) return "D"  + IntToRoman(number - 500);
+            if (number >= 400) return "CD" + IntToRoman(number - 400);
+            if (number >= 100) return "C"  + IntToRoman(number - 100);
+            if (number >= 90) return "XC"  + IntToRoman(number - 90);
+            if (number >= 50) return "L"   + IntToRoman(number - 50);
+            if (number >= 40) return "XL"  + IntToRoman(number - 40);
+            if (number >= 10) return "X"   + IntToRoman(number - 10);
+            if (number >= 9) return "IX"   + IntToRoman(number - 9);
+            if (number >= 5) return "V"    + IntToRoman(number - 5);
+            if (number >= 4) return "IV"   + IntToRoman(number - 4);
+            if (number >= 1) return "I"    + IntToRoman(number - 1);
+            return "";
+        }
+
+        static void Main(string[] args)
+        {
+            int number = 0;
+            bool Input = false;
+
+            while (!Input)  //重複執行直到輸入正確為止
+            {
+                Console.WriteLine("請輸入一個整數: ");
+                try
+                {
+                    number = Convert.ToInt32(Console.ReadLine());
+                    if (1 <= number && number <= 3999)
+                    {
+                        Input = true;                // 讓程式在這裡印出結果後就結束        
+                        Console.WriteLine($"轉換成的羅馬數字為: {IntToRoman(number)}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("輸入的數字不在有效範圍內 (1~3999)");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("輸入的不是有效的(整數)，請再重新輸入。");
+                }
+            }
+            Console.Read();
+        }
+    }
+}
